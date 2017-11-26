499642709d824a4b28cba7d32b782873787303e1
lenghuo <weixupeng999@163.com>
2017-11-27 01:21:10 +0800

    category list for topic

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -11,7 +11,7 @@ class CategoriesController extends Controller
     //
     public function show(Category $category)
     {
-        $topics = Topic::where('category_id',$category->id)->paginate(30);
+        $topics = Topic::where('category_id',$category->id)->paginate(20);
         return view('topics.index',compact('topics','category'));
     }
 }
