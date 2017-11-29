d069ea804afa48e2b3709bfbba5c54f5569b408d
lenghuo <weixupeng999@163.com>
2017-11-29 14:07:49 +0800

    Add queue for translate slug

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,29 +3,23 @@
 namespace App\Observers;
 
 use App\Models\Topic;
-use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
 class TopicObserver
 {
-    public function creating(Topic $topic)
-    {
-        //
-    }
-
-    public function updating(Topic $topic)
-    {
-        //
-    }
-
     public function saving(Topic $topic)
     {
         $topic->body = clean($topic->body,'user_topic_body');
     	$topic->excerpt = make_excerpt($topic->body);
+    }
 
-    	if(! $topic->slug) {
-    	    $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+    public function saved(Topic $topic)
+    {
+        if(! $topic->slug) {
+            //$topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+            dispatch(new TranslateSlug($topic));
         }
     }
 }
