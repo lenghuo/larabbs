70dfa0bf2378de6f9afdeb961537a8a4a63c6949
lenghuo <weixupeng999@163.com>
2018-01-30 04:06:03 +0000

    user store adn verificationCode store

--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,10 @@ use Illuminate\Http\Request;
 
 $api = app('Dingo\Api\Routing\Router');
 
-$api->version('v1',[
-    'namespace' => 'App\Http\Controller\Api',
-],function ($api){
-    $api->post('verificationCodes','VerificationCodesController@store')
+$api->version('v1', [
+    'namespace' => 'App\Http\Controllers\Api'
+], function($api) {
+    // 短信验证码
+    $api->post('verificationCodes', 'VerificationCodesController@store')
         ->name('api.verificationCodes.store');
 });
