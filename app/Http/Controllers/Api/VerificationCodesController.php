76c0bbf17bad2519856823ea388d8aeb4d9c9df3
lenghuo <weixupeng999@163.com>
2018-01-30 04:40:45 +0000

    use can register use phone

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -34,8 +34,8 @@ class VerificationCodesController extends Controller
         ])->setStatusCode(201);*/
         $phone = $request->phone;
 
-        if(!app()->environment('production')) {
-            $code = 1234;
+        if(app()->environment('local')) {
+            $code = '1234';
         } else {
             $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
 
@@ -48,16 +48,15 @@ class VerificationCodesController extends Controller
                 $result = json_decode($response->getBody()->getContents(), true);
                 return $this->response->errorInternal($result['msg'] ?? '短信发送异常');
             }
-
-            $key = 'verificationCode_'.str_random(15);
-            $expiredAt = now()->addMinutes(10);
-            // 缓存验证码 10分钟过期。
-            \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
-
-            return $this->response->array([
-                'key' => $key,
-                'expired_at' => $expiredAt->toDateTimeString(),
-            ])->setStatusCode(201);
         }
+        $key = 'verificationCode_'.str_random(15);
+        $expiredAt = now()->addMinutes(10);
+        // 缓存验证码 10分钟过期。
+        \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+
+        return $this->response->array([
+            'key' => $key,
+            'expired_at' => $expiredAt->toDateTimeString(),
+        ])->setStatusCode(201);
     }
 }
