0bf95523f1b27f906d3bf751b7123a407bd917d6
lenghuo <weixupeng999@163.com>
2018-02-01 09:00:28 +0000

    topic delete api

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -32,4 +32,13 @@ class TopicsController extends Controller
         $topic->update($request->all());
         return $this->response->item($topic, new TopicTransformer());
     }
+
+    public function destory(Topic $topic)
+    {
+        $this->authorize('update',$topic);
+
+        $topic->delete();
+
+        return $this->response->noContent();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -70,6 +70,8 @@ $api->version('v1', [
                 ->name('api.topics.store');
             $api->patch('topics/{topic}','TopicsController@update')
                 ->name('api.topics.update');
+            $api->delete('topics/{topic}','TopicsController@destory')
+                ->name('api.topics.delete');
         });
     });
 });
