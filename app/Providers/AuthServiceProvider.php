8d63bb8b5137217350996bfd3ad4210a6224f278
lenghuo <weixupeng999@163.com>
2018-04-26 09:50:56 +0800

    add passport

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,9 @@ namespace App\Providers;
 use function foo\func;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Carbon\Carbon;
+use Laravel\Passport\Passport;
+use Mockery\Generator\StringManipulation\Pass\Pass;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -28,6 +31,15 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+
+        //Passport 路由
+        Passport::routes();
+
+        // access_token 过期时间
+        Passport::tokensExpireIn(Carbon::now()->addDays(15));
+
+        //refreshToken 过期时间
+        Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
 
         \Horizon::auth(function ($request) {
             return \Auth::user()->hasRole('Founder');
